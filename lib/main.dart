8f3691b788eb5ffe082cb53ca590d1b668458d11
Luciano Araújo Maeda <maedahikaro@gmail.com>
2021-03-23 19:41:47 -0300

    iniciando projeto

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget {
+
+  final perguntaSelecionada = 0;
+
+  void responder(){
+    perguntaSelecionada++;
+    print(perguntaSelecionada);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<String> perguntas = [
+      'Qual a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[perguntaSelecionada]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: responder,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
