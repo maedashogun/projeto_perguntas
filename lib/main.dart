6194836a96ccd6eef90a9650a623948e80b647ad
Luciano Araújo Maeda <maedahikaro@gmail.com>
2021-03-24 13:13:48 -0300

    Tratando exceções

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,12 @@ class _PerguntaAppState extends State<PerguntaApp> {
                   ...respostas.map((t) => Resposta(t, _responder)).toList(),
                 ],
               )
-            : null,
+            : Center(
+              child: Text(
+                'Parabéns!',
+                style: TextStyle(fontSize: 28),
+              ),
+            ),
       ),
     );
   }
